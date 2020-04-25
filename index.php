7d11bc08bdeb52935c6d5171c7568d46788190fb
MickaRz <63889586+MickaRz@users.noreply.github.com>
2020-04-25 12:03:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include 'asset/header.php';
+ ?>
+ <div class="container">
+   <div class="row mt-4">
+     <div class="col-md">
+       <center><h2>Selamat Datang</h2></center>
+       <link rel="stylesheet" type="text/css" href="style.css">
+       <hr class="bg-light">
+     </div>
+   </div>
+ </div>
+     <div class="container">
+        <div class="row mt-4">
+   </div>
+ </div>
+ <div class="row">
+   <div class="col-md-4">
+ </div>
+<body>
+  <div class="col-md-6">
+                <div class="card bg-danger" style="width: 18rem;">
+                     <div class="card-body text-white">
+                         <center><h5 class="card-title">Katalog Game</h5>
+                         <p class="card-text" style="font-size:60px">54</p>
+                         <a href="game/index.php" class="btn fourth">Detail</a></center>
+                     </div>
+                 </div>
+             </div>
+</body>
+ <!-- <div class="col-md-4">
+                <div class="card bg-danger" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Katalog Game</h5>
+                        <p class="card-text" style="font-size:60px">10</p>
+                        <a href="game/index.php" class="text-white">Detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                            <div class="card bg-warning" style="width: 18rem;">
+                                <div class="card-body text-white">
+                                    <h5 class="card-title">Dafta Transaksi</h5>
+                                    <p class="card-text" style="font-size:60px">10</p>
+                                    <a href="transaksi/index.php" class="text-white">Detail <i class="fas fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div> -->
+ <?php
+include 'asset/footer.php';
+  ?>
