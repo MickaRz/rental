b9adb65a174a1fabcc92ee2dbb55cd358e28a79c
MickaRz <63889586+MickaRz@users.noreply.github.com>
2020-04-25 12:04:48 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Silahkan Join</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<!-- <h1>Silahkan Join Terlebih Dahulu</h1> -->
+
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+
+		<form action="proses-login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username">
+
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password">
+
+			<input type="submit" class="tombol_login" <input type="submit" value="Login" name="btnlogin">
+
+			<br/>
+			<br/>
+		</form>
+
+	</div>
+
+
+</body>
+</html>
